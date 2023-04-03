c32895b13e2d0e1eb5e0fb0c4835aeb2eb0162ee
pkishorez <kishore.iiitn@gmail.com>
2023-04-03 14:23:23 +0000

    Deploying to gh-pages from @ pkishorez/chain-reaction@473175468da133d0966103de8539d9022693f5da 🚀

--- _next/static/4JqR76VHMDmFylau9BT0p/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/648-81439329001c86c4.js","static/chunks/pages/index-cede55b49b22e1a8.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
