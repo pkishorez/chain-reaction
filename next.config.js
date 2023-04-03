94743d75b0ed72e61be8fa423e459e40f7baf42b
Kishore <kishore.iiitn@gmail.com>
2023-04-03 19:50:33 +0530

    feat: add service worker.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 const { BASE_PATH } = require("./src/constants");
 
+const withPWA = require("next-pwa")({
+  dest: "public",
+  basePath: BASE_PATH,
+});
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withPWA({
   assetPrefix: BASE_PATH ? `${BASE_PATH}/` : BASE_PATH,
   basePath: BASE_PATH,
-};
+});
---
+++ src/pages/_offline.tsx
@@ -0,0 +1,7 @@
+export default function Offline() {
+  return (
+    <div className="max-w-screen-max mx-auto">
+      <h1 className="mt-10 text-h1">Offline</h1>
+    </div>
+  );
+}
