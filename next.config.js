61ec78470fb0af0375add735dea587066d109fe0
Kishore <kishore.iiitn@gmail.com>
2023-04-03 19:01:25 +0530

    feat: set basePath.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  basePath: "/chain-reaction",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
