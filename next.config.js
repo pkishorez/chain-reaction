13b503b86e90aa1445f532b3b516f08a87790504
Kishore <kishore.iiitn@gmail.com>
2023-04-03 19:10:52 +0530

    fix: github action.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "/";
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  basePath: "/chain-reaction",
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 };
 
 module.exports = nextConfig;
